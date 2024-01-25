a5d2d7f178d12dfa90b72cf03e15657d8efe5860
Cheyenne <hurontechtr1be@huronschools.org>
2024-01-25 18:45:23 -0500

    first commit

---
+++ src/main/java/frc/robot/Subsystems/ClawSubsystem.java
@@ -0,0 +1,25 @@
+package frc.robot.Subsystems;
+
+import edu.wpi.first.math.filter.SlewRateLimiter;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
+import edu.wpi.first.math.kinematics.SwerveModulePosition; 
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.util.WPIUtilJNI;
+//import edu.wpi.first.wpilibj.ADIS16470_IMU;
+//import edu.wpi.first.wpilibj.ADIS16470_IMU.IMUAxis;
+import com.ctre.phoenix.sensors.WPI_PigeonIMU;
+import com.revrobotics.CANSparkMax;
+
+import frc.robot.Constants.DriveConstants;
+import frc.utils.SwerveUtils;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ClawSubsystem {
+    
+
+
+}
