a2caa2d3cddff4db2cf0c8ea36e61b855f0f3641
Cheyenne <hurontechtr1be@huronschools.org>
2024-02-14 19:58:25 -0500

    camra 1 working

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -93,7 +93,7 @@ public class Robot extends TimedRobot {
      // block in order for anything in the Command-based framework to work.
      CommandScheduler.getInstance().run();
 
-     CameraServer.startAutomaticCapture();
+     CameraServer.startAutomaticCapture(); 
 
    }
  
